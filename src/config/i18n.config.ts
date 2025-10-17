0bfd46c094700e5506f48a60993dcd6e9407483b
Han Thi Kim Ngoc <han.thi.kim.ngoc@sun-asterisk.com>
2025-10-17 09:39:38 +0700

    fix: add constant

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -1,8 +1,9 @@
 import { CookieResolver, HeaderResolver, I18nJsonLoader, I18nModule, QueryResolver } from 'nestjs-i18n';
 import * as path from 'path';
+import { LANGUAGE } from 'src/constants/api.constants';
 
 export const i18nConfig = I18nModule.forRoot({
-  fallbackLanguage: 'en',
+  fallbackLanguage: LANGUAGE.EN,
   loaderOptions: {
     path: path.join(__dirname, '../i18n/'),
     watch: true,
